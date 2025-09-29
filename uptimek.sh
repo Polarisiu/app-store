21190c3cf52b5a7444d525c74e18bc4f122105b5
Polaris <fdyrwaas@outlook.com>
2025-09-29 09:00:11 +0800

    Update uptimek.sh

--- uptimek.sh
+++ uptimek.sh
@@ -17,9 +17,9 @@ function get_ip() {
 function menu() {
     clear
     echo -e "${GREEN}=== Uptime-Kuma 管理菜单 ===${RESET}"
-    echo -e "${GREEN}1) 安装/启动${RESET}"
+    echo -e "${GREEN}1) 安装启动${RESET}"
     echo -e "${GREEN}2) 更新${RESET}"
-    echo -e "${GREEN}3) 卸载 (含数据)${RESET}"
+    echo -e "${GREEN}3) 卸载(含数据)${RESET}"
     echo -e "${GREEN}4) 查看日志${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     echo -e "${GREEN}===========================${RESET}"
