908e5ee3544647d1d0a05e75b3ea529477f5c44d
Polaris <fdyrwaas@outlook.com>
2025-09-29 08:50:08 +0800

    Update mootv.sh

--- mootv.sh
+++ mootv.sh
@@ -18,9 +18,9 @@ get_ip() {
 menu() {
     clear
     echo -e "${GREEN}=== MoonTV 管理菜单 ===${RESET}"
-    echo -e "${GREEN}1) 安装/启动${RESET}"
+    echo -e "${GREEN}1) 安装启动${RESET}"
     echo -e "${GREEN}2) 更新${RESET}"
-    echo -e "${GREEN}3) 卸载 (含数据)${RESET}"
+    echo -e "${GREEN}3) 卸载(含数据)${RESET}"
     echo -e "${GREEN}4) 查看日志${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
     echo -e "${GREEN}=======================${RESET}"
