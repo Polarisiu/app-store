f771ad175627e89dfb0d473aaf1b636d35bea65e
Polaris <fdyrwaas@outlook.com>
2025-10-05 19:25:18 +0800

    Update Colo.sh

--- Colo.sh
+++ Colo.sh
@@ -147,7 +147,7 @@ menu() {
     echo -e "${GREEN}4) 查看日志${RESET}"
     echo -e "${GREEN}5) 卸载${RESET}"
     echo -e "${GREEN}0) 退出${RESET}"
-    read -p "请选择: " choice
+    read -p "$(echo -e ${GREEN}请选择:${RESET}) " choice
     case $choice in
         1) install_app ;;
         2) update_app ;;
