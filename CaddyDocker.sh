44c50a79dfd59e04826127b70517d748e4d899b6
Polaris <fdyrwaas@outlook.com>
2025-10-05 17:06:05 +0800

    Update CaddyDocker.sh

--- CaddyDocker.sh
+++ CaddyDocker.sh
@@ -318,7 +318,7 @@ menu() {
         echo -e "${GREEN}10) 查看已配置域名${RESET}"
         echo -e "${GREEN}11) 查看证书状态${RESET}"
         echo -e "${GREEN} 0) 退出${RESET}"
-        read -p "请选择操作[0-11]： " choice
+        read -p "$(echo -e ${GREEN}请选择:${RESET}) " choice
 
         case $choice in
             1) install_app ;;
